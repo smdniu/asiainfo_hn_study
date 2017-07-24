8f0321786826326003c5d4de715af39f367e8e32
wjz001 <1123053342@qq.com>
2017-07-24 17:42:10 +0800

    wjz test

---
+++ src/main/java/com/asiainfo/chapter1/wangjz/java/javaday1/test3.java
@@ -0,0 +1,42 @@
+package com.asiainfo.chapter1.luomin.wangjz.java.javaday1;
+
+/**
+ * Created by wjz123456 on 2017/7/24.
+ */
+public class test3 {
+    public static void main(String args[]){
+        int s=0;
+        for(int a=1;a<100;a++){
+             s=s+a;
+        }
+        System.out.println(s);
+       System.out.println(f(10));
+        System.out.println(o(21,7));
+    }
+    /*
+    递归调用！
+     */
+    public static int f(int n){
+        if( n==1||n==2){
+            return 1;
+        }else{
+            return f(n-1)+f(n-2);
+        }
+    }
+    /*
+    迭代！
+     */
+    public static int o(int a,int b){
+        if(a<=0||b<=0){
+            return 0;
+        }else{
+            while(b>0){
+                int c=a%b;
+                a=b;
+                b=c;
+            }
+            return a;
+        }
+    }
+
+   }
